c7069a160aca08250848a0ecbce9ab2583602f90
Ang107 <kosukenabeta1007@gmail.com>
2024-02-02 23:46:21 +0900

    add

---
+++ abc256/c/main.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque, defaultdict
+from itertools import (
+     accumulate,
+     product,
+     permutations,
+     combinations,
+     combinations_with_replacement
+     )
+import math
+from bisect import bisect_left, insort_left, bisect_right, insort_right
+from pprint import pprint
+from heapq import heapify, heappop, heappush
+
+# product : bit全探索 product(range(2),repeat=n)
+# permutations : 順列全探索
+# combinations : 組み合わせ（重複無し）
+# combinations_with_replacement : 組み合わせ（重複可）
+# from sortedcontainers import SortedSet, SortedList, SortedDict
+sys.setrecursionlimit(10**7)
+around4 = ((-1, 0), (1, 0), (0, -1), (0, 1))  # 上下左右
+around8 = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+inf = float("inf")
+mod = 998244353
+input = lambda: sys.stdin.readline().rstrip()
+P = lambda *x: print(*x)
+PY = lambda: print("Yes")
+PN = lambda: print("No")
+II = lambda: int(input())
+MII = lambda: map(int, input().split())
+LMII = lambda: list(map(int, input().split()))
+
+
+def dlist(*l, fill=0):
+    if len(l) == 1:
+        return [fill] * l[0]
+    ll = l[1:]
+    return [dlist(*ll, fill=fill) for _ in range(l[0])]
+
+h1,h2,h3,w1,w2,w3 = MII()
+for a,b,c,d,e in product(range(1,29),repeat=5):
+    if a + b + c == 
+    

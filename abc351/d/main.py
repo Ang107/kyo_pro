87214000736e97fbcca946997711ef78d3b7b62b
Ang107 <kosukenabeta1007@gmail.com>
2024-04-28 22:40:22 +0900

    add

--- abc351/d/main.py
+++ abc351/d/main.py
@@ -1,99 +1,64 @@
+import time
+
+start = time.perf_counter()
+from collections import deque
 import sys
-from collections import deque, defaultdict
-from itertools import (
-    accumulate,
-    product,
-    permutations,
-    combinations,
-    combinations_with_replacement,
-)
-import math
-from bisect import bisect_left, insort_left, bisect_right, insort_right
-from pprint import pprint
-from heapq import heapify, heappop, heappush
-import string
 
-# 小文字アルファベットのリスト
-alph_s = list(string.ascii_lowercase)
-# 大文字アルファベットのリスト
-alph_l = list(string.ascii_uppercase)
 
-# product : bit全探索 product(range(2),repeat=n)
-# permutations : 順列全探索
-# combinations : 組み合わせ（重複無し）
-# combinations_with_replacement : 組み合わせ（重複可）
-# from sortedcontainers import SortedSet, SortedList, SortedDict
-sys.setrecursionlimit(10**7)
 around4 = ((-1, 0), (1, 0), (0, -1), (0, 1))  # 上下左右
-around8 = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
-inf = float("inf")
-mod = 998244353
+
 input = lambda: sys.stdin.readline().rstrip()
-P = lambda *x: print(*x)
-PY = lambda: print("Yes")
-PN = lambda: print("No")
-SI = lambda: input()
-IS = lambda: input().split()
-II = lambda: int(input())
 MII = lambda: map(int, input().split())
-LMII = lambda: list(map(int, input().split()))
+
 
 h, w = MII()
 s = [input() for _ in range(h)]
 
-no_move = set()
+around_zisyaku = [[False] * w for _ in range(h)]
 for i in range(h):
     for j in range(w):
         if s[i][j] == "#":
             continue
-        flag = False
         for x, y in around4:
             if x + i in range(h) and y + j in range(w):
                 if s[x + i][y + j] == "#":
-                    flag = True
-        if flag:
-            no_move.add((i, j))
+                    around_zisyaku[i][j] = True
 
 
 def dfs(x, y):
-    result = 0
     deq = deque()
-    visited = set()
-
+    visited_local = set()
     deq.append((x, y))
-    visited.add((x, y))
-    result += 1
-    visited_list = []
+    visited_local.add(x * w + y)
     while deq:
         x, y = deq.pop()
         # 動けないなら
-        if (x, y) in no_move:
+        if around_zisyaku[x][y]:
             continue
-        # 動けるなら
-        visited_list.append((x, y))
 
+        # 動けるなら
         for i, j in around4:
             if (
                 x + i in range(h)
                 and y + j in range(w)
-                and (x + i, y + j) not in visited
+                and (x + i) * w + y + j not in visited_local
                 and s[x + i][y + j] != "#"
             ):
-                result += 1
                 deq.append((x + i, y + j))
-                visited.add((x + i, y + j))
-    return result, visited_list
+                visited_local.add((x + i) * w + y + j)
+    return len(visited_local)
 
 
 ans = 1
-visited = set()
-for i in range(h):
-    for j in range(w):
-        if (i, j) not in visited and s[i][j] != "#":
-            result, visited_list = dfs(i, j)
-            for k in visited_list:
-                visited.add(k)
-            ans = max(ans, result)
+import random
 
-            # print([(i + 1, j + 1) for i, j in visited])
+
+while time.perf_counter() - start <= 1.8:
+    i = random.choice(range(h))
+    j = random.choice(range(w))
+    if s[i][j] != "#":
+        result = dfs(i, j)
+        ans = max(ans, result)
+
+
 print(ans)

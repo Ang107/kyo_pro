cc99a092a9e85a86ad3301c14d202d0df0f1410c
Ang107 <kosukenabeta1007@gmail.com>
2024-05-10 20:37:26 +0900

    add

--- AtCoder/ABC/abc267/e/main.py
+++ AtCoder/ABC/abc267/e/main.py
@@ -37,3 +37,200 @@ def dlist(*l, fill=0):
     ll = l[1:]
     return [dlist(*ll, fill=fill) for _ in range(l[0])]
 
+
+# https://github.com/tatyam-prime/SortedSet/blob/main/SortedMultiset.py
+import math
+from bisect import bisect_left, bisect_right
+from typing import Generic, Iterable, Iterator, List, Tuple, TypeVar, Optional
+
+T = TypeVar("T")
+
+
+class SortedMultiset(Generic[T]):
+    BUCKET_RATIO = 16
+    SPLIT_RATIO = 24
+
+    def __init__(self, a: Iterable[T] = []) -> None:
+        "Make a new SortedMultiset from iterable. / O(N) if sorted / O(N log N)"
+        a = list(a)
+        n = self.size = len(a)
+        if any(a[i] > a[i + 1] for i in range(n - 1)):
+            a.sort()
+        num_bucket = int(math.ceil(math.sqrt(n / self.BUCKET_RATIO)))
+        self.a = [
+            a[n * i // num_bucket : n * (i + 1) // num_bucket]
+            for i in range(num_bucket)
+        ]
+
+    def __iter__(self) -> Iterator[T]:
+        for i in self.a:
+            for j in i:
+                yield j
+
+    def __reversed__(self) -> Iterator[T]:
+        for i in reversed(self.a):
+            for j in reversed(i):
+                yield j
+
+    def __eq__(self, other) -> bool:
+        return list(self) == list(other)
+
+    def __len__(self) -> int:
+        return self.size
+
+    def __repr__(self) -> str:
+        return "SortedMultiset" + str(self.a)
+
+    def __str__(self) -> str:
+        s = str(list(self))
+        return "{" + s[1 : len(s) - 1] + "}"
+
+    def _position(self, x: T) -> Tuple[List[T], int, int]:
+        "return the bucket, index of the bucket and position in which x should be. self must not be empty."
+        for i, a in enumerate(self.a):
+            if x <= a[-1]:
+                break
+        return (a, i, bisect_left(a, x))
+
+    def __contains__(self, x: T) -> bool:
+        if self.size == 0:
+            return False
+        a, _, i = self._position(x)
+        return i != len(a) and a[i] == x
+
+    def count(self, x: T) -> int:
+        "Count the number of x."
+        return self.index_right(x) - self.index(x)
+
+    def add(self, x: T) -> None:
+        "Add an element. / O(√N)"
+        if self.size == 0:
+            self.a = [[x]]
+            self.size = 1
+            return
+        a, b, i = self._position(x)
+        a.insert(i, x)
+        self.size += 1
+        if len(a) > len(self.a) * self.SPLIT_RATIO:
+            mid = len(a) >> 1
+            self.a[b : b + 1] = [a[:mid], a[mid:]]
+
+    def _pop(self, a: List[T], b: int, i: int) -> T:
+        ans = a.pop(i)
+        self.size -= 1
+        if not a:
+            del self.a[b]
+        return ans
+
+    def discard(self, x: T) -> bool:
+        "Remove an element and return True if removed. / O(√N)"
+        if self.size == 0:
+            return False
+        a, b, i = self._position(x)
+        if i == len(a) or a[i] != x:
+            return False
+        self._pop(a, b, i)
+        return True
+
+    def lt(self, x: T) -> Optional[T]:
+        "Find the largest element < x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] < x:
+                return a[bisect_left(a, x) - 1]
+
+    def le(self, x: T) -> Optional[T]:
+        "Find the largest element <= x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] <= x:
+                return a[bisect_right(a, x) - 1]
+
+    def gt(self, x: T) -> Optional[T]:
+        "Find the smallest element > x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] > x:
+                return a[bisect_right(a, x)]
+
+    def ge(self, x: T) -> Optional[T]:
+        "Find the smallest element >= x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] >= x:
+                return a[bisect_left(a, x)]
+
+    def __getitem__(self, i: int) -> T:
+        "Return the i-th element."
+        if i < 0:
+            for a in reversed(self.a):
+                i += len(a)
+                if i >= 0:
+                    return a[i]
+        else:
+            for a in self.a:
+                if i < len(a):
+                    return a[i]
+                i -= len(a)
+        raise IndexError
+
+    def pop(self, i: int = -1) -> T:
+        "Pop and return the i-th element."
+        if i < 0:
+            for b, a in enumerate(reversed(self.a)):
+                i += len(a)
+                if i >= 0:
+                    return self._pop(a, ~b, i)
+        else:
+            for b, a in enumerate(self.a):
+                if i < len(a):
+                    return self._pop(a, b, i)
+                i -= len(a)
+        raise IndexError
+
+    def index(self, x: T) -> int:
+        "Count the number of elements < x."
+        ans = 0
+        for a in self.a:
+            if a[-1] >= x:
+                return ans + bisect_left(a, x)
+            ans += len(a)
+        return ans
+
+    def index_right(self, x: T) -> int:
+        "Count the number of elements <= x."
+        ans = 0
+        for a in self.a:
+            if a[-1] > x:
+                return ans + bisect_right(a, x)
+            ans += len(a)
+        return ans
+
+
+n, m = MII()
+a = LMII()
+ed = [[] for _ in range(n)]
+for _ in range(m):
+    u, v = MII()
+    u -= 1
+    v -= 1
+    ed[u].append(v)
+    ed[v].append(u)
+
+# 総和は同じ
+# 貪欲に一番小さいものから？
+cost = [None] * n
+for i in range(n):
+    cost[i] = sum([a[j] for j in ed[i]])
+ss = SortedMultiset([j, i] for i, j in enumerate(cost))
+deleated = set()
+ans = 0
+while ss:
+    # print(ss)
+
+    c, idx = ss.pop(0)
+    ans = max(ans, c)
+    deleated.add(idx)
+    for i in ed[idx]:
+        if i not in deleated:
+            ss.discard([cost[i], i])
+            cost[i] -= a[idx]
+            ss.add([cost[i], i])
+
+print(ans)
--- tmp.py
+++ tmp.py
@@ -1,1 +1,32 @@
-print("ABCABCAB".count("AB"))
+print(sum(int(i) for i in str(int(hex(int(input())), base=36))))
+print(int(str(int(hex(int(input())), base=36)), base=0))
+print(
+    sum(
+        int(i)
+        for i in str(
+            sum(
+                36**j
+                * {
+                    "0": 0,
+                    "1": 1,
+                    "2": 2,
+                    "3": 3,
+                    "4": 4,
+                    "5": 5,
+                    "6": 6,
+                    "7": 7,
+                    "8": 8,
+                    "9": 9,
+                    "A": 10,
+                    "B": 11,
+                    "C": 12,
+                    "D": 13,
+                    "E": 14,
+                    "F": 15,
+                    "x": 33,
+                }[k]
+                for j, k in enumerate(hex(int(input()))[::-1])
+            )
+        )
+    )
+)

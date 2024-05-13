5d564ab6ba4d8c06d9366c642cc560cf0ef64ae3
Ang107 <kosukenabeta1007@gmail.com>
2024-05-13 16:02:27 +0900

    add from note

--- AtCoder/ABC/abc353/e/main.py
+++ AtCoder/ABC/abc353/e/main.py
@@ -1,19 +1,14 @@
 import sys
 
-from bisect import bisect_left, bisect_right
-
-import string
-
 # 外部ライブラリ
 # from sortedcontainers import SortedSet, SortedList, SortedDict
 
 sys.setrecursionlimit(10**7)
-alph_s = tuple(string.ascii_lowercase)
-alph_l = tuple(string.ascii_uppercase)
+
 around4 = ((-1, 0), (1, 0), (0, -1), (0, 1))  # 上下左右
 around8 = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
 inf = float("inf")
-mod = 998244353
+mod = 2**61 - 1
 input = lambda: sys.stdin.readline().rstrip()
 pritn = lambda *x: print(*x)
 PY = lambda: print("Yes")
@@ -25,31 +20,89 @@ MII = lambda: map(int, input().split())
 LMII = lambda: list(map(int, input().split()))
 
 n = int(input())
-s = list(map(list, input().split()))
+s = input().split()
+from collections import defaultdict
 
-s.sort()
+dd = defaultdict(int)
 ans = 0
+mod = 2**61 - 1
+b = 31
+for i in s:
+    hash = 0
+    for j in i:
+        hash = (hash + (ord(j) - ord("a") + 1)) % mod
+        ans += dd[hash]
+        # print(i, hash)
+        dd[hash] += 1
+        hash *= b
+        hash %= mod
+pritn(ans)
 
-for index, i in enumerate(s):
-    tmp = i[:]
-    ok = index
-    ng = n
-    for j in range(len(i)):
 
-        def isOK(mid):
-            return j < len(s[mid]) and i[j] == s[mid][j]
+# class Node:
+#     def __init__(self):
+#         self.cnt = 0
+#         self.child = [None] * 26
 
-        def meguru(ng, ok):
-            while abs(ok - ng) > 1:
-                mid = (ok + ng) // 2
-                if isOK(mid):
-                    ok = mid
-                else:
-                    ng = mid
-            return ok
 
-        tmp = meguru(ng, ok)
-        ans += tmp - index
-        ng = tmp + 1
+# class TrieTree:
+#     def __init__(self):
+#         self.root = Node()
 
-print(ans)
+#     def add(self, s: str):
+#         node = self.root
+#         for i in s:
+#             v = ord(i) - ord("a")
+#             if node.child[v] == None:
+#                 node.child[v] = Node()
+#             node = node.child[v]
+#             node.cnt += 1
+
+#     def get_sum(self, s: str) -> int:
+#         result = 0
+#         node = self.root
+#         for i in s:
+#             v = ord(i) - ord("a")
+#             if node.child[v] == None:
+#                 return result
+#             node = node.child[v]
+#             result += node.cnt
+#         return result
+
+
+# tt = TrieTree()
+# for i in s:
+#     ans += tt.get_sum(i)
+#     tt.add(i)
+#     # print(ans)
+
+
+# print(ans)
+
+
+# s.sort()
+# ans = 0
+
+# for index, i in enumerate(s):
+#     tmp = i[:]
+#     ok = index
+#     ng = n
+#     for j in range(len(i)):
+
+#         def isOK(mid):
+#             return j < len(s[mid]) and i[j] == s[mid][j]
+
+#         def meguru(ng, ok):
+#             while abs(ok - ng) > 1:
+#                 mid = (ok + ng) // 2
+#                 if isOK(mid):
+#                     ok = mid
+#                 else:
+#                     ng = mid
+#             return ok
+
+#         tmp = meguru(ng, ok)
+#         ans += tmp - index
+#         ng = tmp + 1
+
+# print(ans)
--- tmp.py
+++ tmp.py
@@ -1,32 +1,8 @@
-print(sum(int(i) for i in str(int(hex(int(input())), base=36))))
-print(int(str(int(hex(int(input())), base=36)), base=0))
-print(
-    sum(
-        int(i)
-        for i in str(
-            sum(
-                36**j
-                * {
-                    "0": 0,
-                    "1": 1,
-                    "2": 2,
-                    "3": 3,
-                    "4": 4,
-                    "5": 5,
-                    "6": 6,
-                    "7": 7,
-                    "8": 8,
-                    "9": 9,
-                    "A": 10,
-                    "B": 11,
-                    "C": 12,
-                    "D": 13,
-                    "E": 14,
-                    "F": 15,
-                    "x": 33,
-                }[k]
-                for j, k in enumerate(hex(int(input()))[::-1])
-            )
-        )
-    )
-)
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+ans = 0
+from bisect import bisect_right
+
+for i in range(n):
+    ans += bisect_right(a, a[i] + k) - i - 1
+print(ans)

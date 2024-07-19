19ca7f11cffc6d375e00d03a9584475f0f3fbb6e
Ang107 <kosukenabeta1007@gmail.com>
2024-07-19 20:42:54 +0900

    add note

--- AtCoder/Practice/tessoku-book/b69/main.py
+++ AtCoder/Practice/tessoku-book/b69/main.py
@@ -1,10 +1,17 @@
 import sys
 from collections import deque, defaultdict
-from itertools import accumulate, product, permutations, combinations, combinations_with_replacement
+from itertools import (
+    accumulate,
+    product,
+    permutations,
+    combinations,
+    combinations_with_replacement,
+)
 import math
 from bisect import bisect_left, insort_left, bisect_right, insort_right
 from pprint import pprint
 from heapq import heapify, heappop, heappush
+
 # product : bit全探索 product(range(2),repeat=n)
 # permutations : 順列全探索
 # combinations : 組み合わせ（重複無し）
@@ -12,19 +19,201 @@ from heapq import heapify, heappop, heappush
 # from sortedcontainers import SortedSet, SortedList, SortedDict
 sys.setrecursionlimit(10**7)
 around4 = ((-1, 0), (1, 0), (0, -1), (0, 1))  # 上下左右
-around8 = ((-1, -1), (-1, 0), (-1, 1), (0, -1),
-           (0, 1), (1, -1), (1, 0), (1, 1))
-inf = float('inf')
+around8 = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+inf = float("inf")
 deq = deque()
 dd = defaultdict()
 mod = 998244353
 
 
-def Pr(x): return print(x)
-def PY(): return print("Yes")
-def PN(): return print("No")
-def I(): return input()
-def II(): return int(input())
-def MII(): return map(int, input().split())
-def LMII(): return list(map(int, input().split()))
-def is_not_Index_Er(x, y, h, w): return 0 <= x < h and 0 <= y < w  # 範囲外参照
+def Pr(x):
+    return print(x)
+
+
+def PY():
+    return print("Yes")
+
+
+def PN():
+    return print("No")
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LMII():
+    return list(map(int, input().split()))
+
+
+def is_not_Index_Er(x, y, h, w):
+    return 0 <= x < h and 0 <= y < w  # 範囲外参照
+
+
+from typing import NamedTuple, Optional, List, cast
+
+
+class MFGraph:
+    class Edge(NamedTuple):
+        src: int
+        dst: int
+        cap: int
+        flow: int
+
+    class _Edge:
+        def __init__(self, dst: int, cap: int) -> None:
+            self.dst = dst
+            self.cap = cap
+            self.rev: Optional[MFGraph._Edge] = None
+
+    def __init__(self, n: int) -> None:
+        self._n = n
+        self._g: List[List[MFGraph._Edge]] = [[] for _ in range(n)]
+        self._edges: List[MFGraph._Edge] = []
+
+    def add_edge(self, src: int, dst: int, cap: int) -> int:
+        assert 0 <= src < self._n
+        assert 0 <= dst < self._n
+        assert 0 <= cap
+        m = len(self._edges)
+        e = MFGraph._Edge(dst, cap)
+        re = MFGraph._Edge(src, 0)
+        e.rev = re
+        re.rev = e
+        self._g[src].append(e)
+        self._g[dst].append(re)
+        self._edges.append(e)
+        return m
+
+    def get_edge(self, i: int) -> Edge:
+        assert 0 <= i < len(self._edges)
+        e = self._edges[i]
+        re = cast(MFGraph._Edge, e.rev)
+        return MFGraph.Edge(re.dst, e.dst, e.cap + re.cap, re.cap)
+
+    def edges(self) -> List[Edge]:
+        return [self.get_edge(i) for i in range(len(self._edges))]
+
+    def change_edge(self, i: int, new_cap: int, new_flow: int) -> None:
+        assert 0 <= i < len(self._edges)
+        assert 0 <= new_flow <= new_cap
+        e = self._edges[i]
+        e.cap = new_cap - new_flow
+        assert e.rev is not None
+        e.rev.cap = new_flow
+
+    def flow(self, s: int, t: int, flow_limit: Optional[int] = None) -> int:
+        assert 0 <= s < self._n
+        assert 0 <= t < self._n
+        assert s != t
+        if flow_limit is None:
+            flow_limit = cast(int, sum(e.cap for e in self._g[s]))
+
+        current_edge = [0] * self._n
+        level = [0] * self._n
+
+        def fill(arr: List[int], value: int) -> None:
+            for i in range(len(arr)):
+                arr[i] = value
+
+        def bfs() -> bool:
+            fill(level, self._n)
+            queue = []
+            q_front = 0
+            queue.append(s)
+            level[s] = 0
+            while q_front < len(queue):
+                v = queue[q_front]
+                q_front += 1
+                next_level = level[v] + 1
+                for e in self._g[v]:
+                    if e.cap == 0 or level[e.dst] <= next_level:
+                        continue
+                    level[e.dst] = next_level
+                    if e.dst == t:
+                        return True
+                    queue.append(e.dst)
+            return False
+
+        def dfs(lim: int) -> int:
+            stack = []
+            edge_stack: List[MFGraph._Edge] = []
+            stack.append(t)
+            while stack:
+                v = stack[-1]
+                if v == s:
+                    flow = min(lim, min(e.cap for e in edge_stack))
+                    for e in edge_stack:
+                        e.cap -= flow
+                        assert e.rev is not None
+                        e.rev.cap += flow
+                    return flow
+                next_level = level[v] - 1
+                while current_edge[v] < len(self._g[v]):
+                    e = self._g[v][current_edge[v]]
+                    re = cast(MFGraph._Edge, e.rev)
+                    if level[e.dst] != next_level or re.cap == 0:
+                        current_edge[v] += 1
+                        continue
+                    stack.append(e.dst)
+                    edge_stack.append(re)
+                    break
+                else:
+                    stack.pop()
+                    if edge_stack:
+                        edge_stack.pop()
+                    level[v] = self._n
+            return 0
+
+        flow = 0
+        while flow < flow_limit:
+            if not bfs():
+                break
+            fill(current_edge, 0)
+            while flow < flow_limit:
+                f = dfs(flow_limit - flow)
+                flow += f
+                if f == 0:
+                    break
+        return flow
+
+    def min_cut(self, s: int) -> List[bool]:
+        visited = [False] * self._n
+        stack = [s]
+        visited[s] = True
+        while stack:
+            v = stack.pop()
+            for e in self._g[v]:
+                if e.cap > 0 and not visited[e.dst]:
+                    visited[e.dst] = True
+                    stack.append(e.dst)
+        return visited
+
+
+n, m = MII()
+c = [input() for _ in range(n)]
+mf = MFGraph(n + 26)
+s = 0
+t = n + 25
+for i in range(1, n + 1):
+    mf.add_edge(s, i, 10)
+for i in range(n + 1, n + 25):
+    mf.add_edge(i, t, m)
+for i in range(n):
+    for j in range(24):
+        if c[i][j] == "1":
+            mf.add_edge(1 + i, 1 + n + j, 1)
+
+rslt = mf.flow(0, n + 25)
+if rslt == m * 24:
+    PY()
+else:
+    PN()

86e519c863808e97e6091eb38ef54bb815edc0de
Ang107 <kosukenabeta1007@gmail.com>
2024-07-20 20:49:59 +0900

    add desktop

--- CodeForces/959_div2/C/main.py
+++ CodeForces/959_div2/C/main.py
@@ -37,27 +37,46 @@ ans = []
 for _ in range(t):
     n, x = MII()
     a = LMII()
-    acc = [0] + list(accumulate(a))
-    to = []
-    start = [0] * n
-    end = [0] * n
-    for i in range(n):
-        tmp = bisect_right(acc, x + acc[i]) - 1
-        to.append(tmp)
-
-    rslt = (n + 1) * n // 2
-    for i in range(n):
-        if to[i] < n:
-            end[to[i]] += 1
-            start[i] += 1
-            rslt -= 1
-
-    for i in range(n - 1):
-        if end[i] and start[i + 1]:
-            rslt -= end[i]
-            end[to[i + 1]] += end[i]
-
+    r = n
+    # dp[i] = l=iとしたときに、f(l,r) == 0を満たすrの個数
+    dp = [0] * (n + 1)
+    sum_ = 0
+    rslt = 0
+    for i in reversed(range(n)):
+        sum_ += a[i]
+        print(sum_)
+        while sum_ - a[r - 1] > x:
+            print(i, sum_, r)
+            r -= 1
+            sum_ -= a[r]
+        if sum_ <= x:
+            dp[i] = 0
+        else:
+            dp[i] = dp[r] + 1
+        rslt += n - i - dp[i]
+        print(dp)
     ans.append(rslt)
+    # acc = [0] + list(accumulate(a))
+    # to = []
+    # start = [0] * n
+    # end = [0] * n
+    # for i in range(n):
+    #     tmp = bisect_right(acc, x + acc[i]) - 1
+    #     to.append(tmp)
+
+    # rslt = (n + 1) * n // 2
+    # for i in range(n):
+    #     if to[i] < n:
+    #         end[to[i]] += 1
+    #         start[i] += 1
+    #         rslt -= 1
+
+    # for i in range(n - 1):
+    #     rslt -= end[i] * start[i + 1]
+    #     if to[i + 1] < n:
+    #         end[to[i + 1]] += end[i]
+
+    # ans.append(rslt)
 
 
 for i in ans:

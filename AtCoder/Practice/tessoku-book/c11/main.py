49947cf514f6ae4cf2512b53829b1dccf5621439
Ang107 <kosukenabeta1007@gmail.com>
2024-08-02 21:28:00 +0900

    from note

--- AtCoder/Practice/tessoku-book/c11/main.py
+++ AtCoder/Practice/tessoku-book/c11/main.py
@@ -1,10 +1,17 @@
 import sys
 from collections import deque, defaultdict
-from itertools import accumulate, product, permutations, combinations, combinations_with_replacement
+from itertools import (
+    accumulate,
+    product,
+    permutations,
+    combinations,
+    combinations_with_replacement,
+)
 import math
 from bisect import bisect_left, insort_left, bisect_right, insort_right
 from pprint import pprint
 from heapq import heapify, heappop, heappush
+
 # product : bit全探索 product(range(2),repeat=n)
 # permutations : 順列全探索
 # combinations : 組み合わせ（重複無し）
@@ -12,19 +19,118 @@ from heapq import heapify, heappop, heappush
 # from sortedcontainers import SortedSet, SortedList, SortedDict
 sys.setrecursionlimit(10**7)
 around4 = ((-1, 0), (1, 0), (0, -1), (0, 1))  # 上下左右
-around8 = ((-1, -1), (-1, 0), (-1, 1), (0, -1),
-           (0, 1), (1, -1), (1, 0), (1, 1))
-inf = float('inf')
+around8 = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+inf = float("inf")
 deq = deque()
 dd = defaultdict()
 mod = 998244353
 
 
-def Pr(x): return print(x)
-def PY(): return print("Yes")
-def PN(): return print("No")
-def I(): return input()
-def II(): return int(input())
-def MII(): return map(int, input().split())
-def LMII(): return list(map(int, input().split()))
-def is_not_Index_Er(x, y, h, w): return 0 <= x < h and 0 <= y < w  # 範囲外参照
+def Pr(x):
+    return print(x)
+
+
+def PY():
+    return print("Yes")
+
+
+def PN():
+    return print("No")
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LMII():
+    return list(map(int, input().split()))
+
+
+def is_not_Index_Er(x, y, h, w):
+    return 0 <= x < h and 0 <= y < w  # 範囲外参照
+
+
+n, k = MII()
+a = LMII()
+
+
+# 当選に必要な最低票数がmidの時に、何人が当選するか
+def isOK1(mid):
+    def isOK2(mid2):
+        if mid2 == 0:
+            return True
+        else:
+            if i / mid2 >= mid:
+                return True
+            else:
+                return False
+        pass
+
+    def meguru2(ng, ok):
+        while abs(ok - ng) > 1:
+            mid = (ok + ng) // 2
+            if isOK2(mid):
+                ok = mid
+            else:
+                ng = mid
+        return ok
+
+    rslt = 0
+    for i in a:
+        rslt += meguru2(i, 0)
+    return rslt >= k
+
+    pass
+
+
+def meguru1(ng, ok):
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) // 2
+        if isOK1(mid):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
+tmp = meguru1(10**9 + 1, 2)
+
+
+# print(tmp)
+def f(mid):
+    def isOK2(mid2):
+        if mid2 == 0:
+            return True
+        else:
+            if i / mid2 >= mid:
+                return True
+            else:
+                return False
+
+    def meguru2(ng, ok):
+        while abs(ok - ng) > 1:
+            mid = (ok + ng) // 2
+            if isOK2(mid):
+                ok = mid
+            else:
+                ng = mid
+        return ok
+
+    rslt = []
+    for i in a:
+        rslt.append(meguru2(i, 0))
+    return rslt
+
+    pass
+
+
+ans = f(tmp)
+print(*ans)

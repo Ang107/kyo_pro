15d0ecb723b8ac389c8143bd32092e28114cb282
Ang107 <kosukenabeta1007@gmail.com>
2024-08-12 19:26:00 +0900

    add from note

--- AtCoder/ABC/abc366/e/main.py
+++ AtCoder/ABC/abc366/e/main.py
@@ -1,5 +1,5 @@
 import sys
-from bisect import bisect_left
+from bisect import bisect_left, bisect_right
 
 input = lambda: sys.stdin.readline().rstrip()
 II = lambda: int(input())
@@ -13,105 +13,136 @@ x_list = [i for i, j in xy]
 y_list = [j for i, j in xy]
 x_list.sort()
 y_list.sort()
-x_l = [0]
-for i in range(1, n):
-    x_l.append(x_l[-1] + abs(x_list[i] - x_list[0]))
-x_r = [0]
-for i in reversed(range(n - 1)):
-    x_r.append(x_r[-1] + abs(x_list[i] - x_list[-1]))
-x_r = x_r[::-1]
-y_l = [0]
-for i in range(1, n):
-    y_l.append(y_l[-1] + abs(y_list[i] - y_list[0]))
-y_r = [0]
-for i in reversed(range(n - 1)):
-    y_r.append(y_r[-1] + abs(y_list[i] - y_list[-1]))
-y_r = y_r[::-1]
+tmp = 0
+for x in x_list:
+    tmp += x - (-2000000)
+dis_x = [tmp]
+r = -1
+for x in range(-1999999, 2000001):
+    while r + 1 < n and x_list[r + 1] <= x - 1:
+        r += 1
+    dis_x.append(dis_x[-1] + (r + 1) - (n - (r + 1)))
 
-
-def f(xy, num):
-    if xy == "x":
-        if num <= x_list[0]:
-            return x_l[-1] + abs(x_list[0] - num) * n
-        elif x_list[-1] <= num:
-            return x_r[0] + abs(x_list[-1] - num) * n
-        else:
-            rslt = 0
-            tmp = bisect_left(x_list, num)
-            rslt += x_l[-1] - x_l[tmp - 1] - abs(x_list[0] - num) * (n - tmp)
-            rslt += x_r[0] - x_r[tmp] - abs(x_list[-1] - num) * tmp
-            return rslt
-    else:
-        if num <= y_list[0]:
-            return y_l[-1] + abs(y_list[0] - num) * n
-        elif y_list[-1] <= num:
-            return y_r[0] + abs(y_list[-1] - num) * n
-        else:
-            rslt = 0
-            tmp = bisect_left(y_list, num)
-            rslt += y_l[-1] - y_l[tmp - 1] - abs(y_list[0] - num) * (n - tmp)
-            rslt += y_r[0] - y_r[tmp] - abs(y_list[-1] - num) * tmp
-            return rslt
-
-
-memo_x = [-1] * (4 * 10**6 + 3)
-for i in range(4 * 10**6 + 3):
-    memo_x[i] = f("x", i - 2000001)
-memo_y = [-1] * (4 * 10**6 + 3)
-for i in range(4 * 10**6 + 3):
-    memo_y[i] = f("y", i - 2000001)
-
-
-def isOK(mid):
-    return tmp + memo_y[mid + 2000001] <= d
-
-
-def meguru(ng, ok):
-    while abs(ok - ng) > 1:
-        mid = (ok + ng) // 2
-        if isOK(mid):
-            ok = mid
-        else:
-            ng = mid
-    return ok
-
-
-if n % 2 == 1:
-    x_mean = x_list[n // 2]
-    y_mean = y_list[n // 2]
-else:
-    x_mean = (x_list[n // 2] + x_list[n // 2 - 1]) // 2
-    y_mean = (y_list[n // 2] + y_list[n // 2 - 1]) // 2
-
-
-i = 0
-prv1 = -2 * 10**6
-prv2 = 2 * 10**6
-
-while True:
-    tmp = memo_x[x_mean + i + 2000001]
-    if tmp + memo_y[y_mean + 2000001] <= d:
-        tmp1 = meguru(prv1 - 1, y_mean)
-        tmp2 = meguru(prv2 + 1, y_mean)
-        prv1 = tmp1
-        prv2 = tmp2
-        ans += max(0, tmp2 - tmp1 + 1)
-    else:
-        break
-    i += 1
-i = -1
-prv1 = -2 * 10**6
-prv2 = 2 * 10**6
-while True:
-    tmp = memo_x[x_mean + i + 2000001]
-    if tmp + memo_y[y_mean + 2000001] <= d:
-        tmp1 = meguru(prv1 - 1, y_mean)
-        tmp2 = meguru(prv2 + 1, y_mean)
-        prv1 = tmp1
-        prv2 = tmp2
-        ans += max(0, tmp2 - tmp1 + 1)
-
-    else:
-        break
-    i -= 1
+tmp = 0
+for y in y_list:
+    tmp += y - (-2000000)
+dis_y = [tmp]
+r = -1
+for y in range(-1999999, 2000001):
+    while r + 1 < n and y_list[r + 1] <= y - 1:
+        r += 1
+    dis_y.append(dis_y[-1] + (r + 1) - (n - (r + 1)))
+tmp = 0
+ans = 0
+dis_x.sort()
+dis_y.sort()
+prv = len(dis_x)
+r = -1
+for i in dis_x[::-1]:
+    while r + 1 < len(dis_y) and i + dis_y[r + 1] <= d:
+        r += 1
+    ans += r + 1
 print(ans)
+
+# x_l = [0]
+# for i in range(1, n):
+#     x_l.append(x_l[-1] + abs(x_list[i] - x_list[0]))
+# x_r = [0]
+# for i in reversed(range(n - 1)):
+#     x_r.append(x_r[-1] + abs(x_list[i] - x_list[-1]))
+# x_r = x_r[::-1]
+# y_l = [0]
+# for i in range(1, n):
+#     y_l.append(y_l[-1] + abs(y_list[i] - y_list[0]))
+# y_r = [0]
+# for i in reversed(range(n - 1)):
+#     y_r.append(y_r[-1] + abs(y_list[i] - y_list[-1]))
+# y_r = y_r[::-1]
+
+
+# def f(xy, num):
+#     if xy == "x":
+#         if num <= x_list[0]:
+#             return x_l[-1] + abs(x_list[0] - num) * n
+#         elif x_list[-1] <= num:
+#             return x_r[0] + abs(x_list[-1] - num) * n
+#         else:
+#             rslt = 0
+#             tmp = bisect_left(x_list, num)
+#             rslt += x_l[-1] - x_l[tmp - 1] - abs(x_list[0] - num) * (n - tmp)
+#             rslt += x_r[0] - x_r[tmp] - abs(x_list[-1] - num) * tmp
+#             return rslt
+#     else:
+#         if num <= y_list[0]:
+#             return y_l[-1] + abs(y_list[0] - num) * n
+#         elif y_list[-1] <= num:
+#             return y_r[0] + abs(y_list[-1] - num) * n
+#         else:
+#             rslt = 0
+#             tmp = bisect_left(y_list, num)
+#             rslt += y_l[-1] - y_l[tmp - 1] - abs(y_list[0] - num) * (n - tmp)
+#             rslt += y_r[0] - y_r[tmp] - abs(y_list[-1] - num) * tmp
+#             return rslt
+
+
+# memo_x = [-1] * (4 * 10**6 + 3)
+# for i in range(4 * 10**6 + 3):
+#     memo_x[i] = f("x", i - 2000001)
+# memo_y = [-1] * (4 * 10**6 + 3)
+# for i in range(4 * 10**6 + 3):
+#     memo_y[i] = f("y", i - 2000001)
+
+
+# def isOK(mid):
+#     return tmp + memo_y[mid + 2000001] <= d
+
+
+# def meguru(ng, ok):
+#     while abs(ok - ng) > 1:
+#         mid = (ok + ng) // 2
+#         if isOK(mid):
+#             ok = mid
+#         else:
+#             ng = mid
+#     return ok
+
+
+# if n % 2 == 1:
+#     x_mean = x_list[n // 2]
+#     y_mean = y_list[n // 2]
+# else:
+#     x_mean = (x_list[n // 2] + x_list[n // 2 - 1]) // 2
+#     y_mean = (y_list[n // 2] + y_list[n // 2 - 1]) // 2
+
+
+# i = 0
+# prv1 = -2 * 10**6
+# prv2 = 2 * 10**6
+
+# while True:
+#     tmp = memo_x[x_mean + i + 2000001]
+#     if tmp + memo_y[y_mean + 2000001] <= d:
+#         tmp1 = meguru(prv1 - 1, y_mean)
+#         tmp2 = meguru(prv2 + 1, y_mean)
+#         prv1 = tmp1
+#         prv2 = tmp2
+#         ans += max(0, tmp2 - tmp1 + 1)
+#     else:
+#         break
+#     i += 1
+# i = -1
+# prv1 = -2 * 10**6
+# prv2 = 2 * 10**6
+# while True:
+#     tmp = memo_x[x_mean + i + 2000001]
+#     if tmp + memo_y[y_mean + 2000001] <= d:
+#         tmp1 = meguru(prv1 - 1, y_mean)
+#         tmp2 = meguru(prv2 + 1, y_mean)
+#         prv1 = tmp1
+#         prv2 = tmp2
+#         ans += max(0, tmp2 - tmp1 + 1)
+
+#     else:
+#         break
+#     i -= 1
+# print(ans)
--- AtCoder/ARC/arc182/a/main.py
+++ AtCoder/ARC/arc182/a/main.py
@@ -5,10 +5,12 @@ from itertools import permutations
 from itertools import product
 from itertools import combinations
 from itertools import combinations_with_replacement
-from math import ceil, floor, log, log2, sqrt, gcd, lcm
+
+# from math import ceil, floor, log, log2, sqrt, gcd, lcm
 from bisect import bisect_left, bisect_right
 from heapq import heapify, heappop, heappush
-from functools import cache
+
+# from functools import cache
 from string import ascii_lowercase, ascii_uppercase
 
 DEBUG = True
@@ -36,54 +38,75 @@ LMII = lambda: list(map(int, input().split()))
 
 n, q = MII()
 pv = [LMII() for _ in range(q)]
-v = [j for i, j in pv]
-tmp = {j: i for i, j in enumerate([0] + sorted(set(v)))}
-# 座圧
+cand = [[True, True] for _ in range(q)]
 for i in range(q):
-    pv[i][0] -= 1
-    pv[i][1] = tmp[pv[i][1]]
-# 左からi番目までで、最大値がj
-dp_l = [[0] * len(tmp) for _ in range(n)]
-dp_r = [[0] * len(tmp) for _ in range(n)]
-for i in range(n):
-    dp_l[i][0] = 1
-    dp_r[i][0] = 1
-print(pv)
-for p, v in pv:
-    # 左側を採用する場合
-    cnt1 = 0
-    for i in range(v + 1):
-        cnt1 += dp_l[p][i]
-        cnt1 %= mod
+    for j in range(i + 1, q):
+        if pv[i][1] > pv[j][1]:
+            if pv[i][0] == pv[j][0]:
+                cand[i][0] &= False
+                cand[i][1] &= False
+                cand[j][0] &= False
+                cand[j][1] &= False
+            elif pv[i][0] < pv[j][0]:
+                cand[i][1] &= False
+                cand[j][0] &= False
+            elif pv[i][0] > pv[j][0]:
+                cand[i][0] &= False
+                cand[j][1] &= False
+ans = 1
+for i in range(q):
+    ans *= cand[i].count(True)
+    ans %= mod
 
-    # 右側を採用する場合
-    cnt2 = 0
-    for i in range(v + 1):
-        cnt2 += dp_r[p][i]
-        cnt2 %= mod
-    for i in range(p + 1):
-        dp_l[i][v] += cnt1
-        dp_l[i][v] %= mod
-
-    for i in range(p, n):
-        dp_r[i][v] += cnt2
-        dp_r[i][v] %= mod
-
-    for i in range(v):
-
-        for j in range(p, n):
-            dp_l[j][i] = 0
-        for j in range(p + 1):
-            dp_r[j][i] = 0
-
-    deb(dp_l)
-    deb(dp_r)
-ans = 0
-for i in range(n):
-    tmp = 0
-    tmp = sum(dp_l[i]) * sum(dp_r[i])
-    # for j in range(len(dp_l[0])):
-    #     tmp += min(dp_l[i][j], dp_r[i][j])
-    #     tmp %= mod
-    ans = max(ans, tmp)
 print(ans)
+# v = [j for i, j in pv]
+# tmp = {j: i for i, j in enumerate([0] + sorted(set(v)))}
+# # 座圧
+# for i in range(q):
+#     pv[i][0] -= 1
+#     pv[i][1] = tmp[pv[i][1]]
+# # 左からi番目までで、最大値がj
+# dp_l = [[0] * len(tmp) for _ in range(n)]
+# dp_r = [[0] * len(tmp) for _ in range(n)]
+# for i in range(n):
+#     dp_l[i][0] = 1
+#     dp_r[i][0] = 1
+# print(pv)
+# for p, v in pv:
+#     # 左側を採用する場合
+#     cnt1 = 0
+#     for i in range(v + 1):
+#         cnt1 += dp_l[p][i]
+#         cnt1 %= mod
+
+#     # 右側を採用する場合
+#     cnt2 = 0
+#     for i in range(v + 1):
+#         cnt2 += dp_r[p][i]
+#         cnt2 %= mod
+#     for i in range(p + 1):
+#         dp_l[i][v] += cnt1
+#         dp_l[i][v] %= mod
+
+#     for i in range(p, n):
+#         dp_r[i][v] += cnt2
+#         dp_r[i][v] %= mod
+
+#     for i in range(v):
+
+#         for j in range(p, n):
+#             dp_l[j][i] = 0
+#         for j in range(p + 1):
+#             dp_r[j][i] = 0
+
+#     deb(dp_l)
+#     deb(dp_r)
+# ans = 0
+# for i in range(n):
+#     tmp = 0
+#     tmp = sum(dp_l[i]) * sum(dp_r[i])
+#     # for j in range(len(dp_l[0])):
+#     #     tmp += min(dp_l[i][j], dp_r[i][j])
+#     #     tmp %= mod
+#     ans = max(ans, tmp)
+# print(ans)
